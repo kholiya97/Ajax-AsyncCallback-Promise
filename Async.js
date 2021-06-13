b1693faa04601602cacf68e203d6afcd6744c909
kholiya97 <80846923+kholiya97@users.noreply.github.com>
2021-06-13 17:59:48 +0530

    [Himanshu] Add : Demonstrated the Async nature of java script execution

--- Async.js
+++ Async.js
@@ -1,6 +1,6 @@
 function showTime(){ 
     const date=new Date();
-    return date.getHours()+"Hrs:"+date.getMinutes()+"Mins:"+date.getSeconds()+"Secs";
+    return date.getHours()+"Hrs:"+date.getMinutes()+"Mins:"+date.getSeconds()+"Secs"; 
 }
 function showSessionExpire(){
     console.log("Activity-B: Your session expired at" +showTime());
